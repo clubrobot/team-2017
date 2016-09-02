57098091b5c391350499d6d85cb18d42ea55e151
Ulysse Darmet <darmet.ulysse@gmail.com>
2016-09-02 21:44:21 +0200

    Wait for the Arduino to send its UUID

--- libraries/robotcom/robotcom.py
+++ libraries/robotcom/robotcom.py
@@ -10,25 +10,31 @@ BAUDRATE			= 115200
 COMMAND_START_BYTE	= b'R'
 OUTPUT_START_BYTE	= b'A'
 
-UUID_GET_OPCODE		= 0x00
-UUID_SET_OPCODE		= 0x01
+GET_UUID_OPCODE		= 0x00
+SET_UUID_OPCODE		= 0x01
 
 class RobotCom:
 
 	def __init__(self, port):
-		self.serial			= serial.Serial(baudrate = BAUDRATE)
+		self.serial			= serial.Serial(baudrate	= BAUDRATE,
+											bytesize	= serial.EIGHTBITS,
+											parity		= serial.PARITY_NONE,
+											stopbits	= serial.STOPBITS_ONE)
 		self.serial.port	= port
+		self.serial.timeout	= 1
 
-	def connect(self):
+	def connect(self, timeout = 2):
 		self.serial.open()
 		self.daemon = Daemon(self.serial)
 		self.daemon.daemon = True
 		self.daemon.start()
-	
+		if self.poll(GET_UUID_OPCODE, timeout) is None:
+			raise TimeoutError('\'{}\' is mute. It may not be an Arduino or it\'s sketch may not be correctly loaded.'.format(self.serial.port))
+
 	def disconnect(self):
-		self.serial.close()
 		self.daemon.stop_event.set()
-		self.daemon.join() # Is this needed?
+		self.daemon.join()
+		self.serial.close()
 
 	def send(self, opcode, *params):
 		prefix = COMMAND_START_BYTE
@@ -47,8 +53,7 @@ class RobotCom:
 				content += bytes(param)
 
 		prefix += bytes([len(content)])
-		self.serial.write(prefix)
-		self.serial.write(content)
+		return self.serial.write(prefix + content)
 	
 	def poll(self, opcode, timeout = 0):
 		queue = self.daemon.get_queue(opcode)
@@ -63,22 +68,22 @@ class RobotCom:
 			pass
 	
 	def get_uuid(self, timeout = 1):
-		self.flush(UUID_GET_OPCODE)
-		self.send(UUID_GET_OPCODE)
-		uuid = self.poll(UUID_GET_OPCODE, timeout)
+		self.flush(GET_UUID_OPCODE)
+		self.send(GET_UUID_OPCODE)
+		uuid = self.poll(GET_UUID_OPCODE, timeout)
 		if uuid is not None:
 			return uuid.decode('charmap')
 		else:
 			return None
 
 	def set_uuid(self, uuid):
-		self.send(UUID_SET_OPCODE, uuid)
+		return self.send(SET_UUID_OPCODE, uuid)
 
 	def __enter__(self):
 		self.connect()
 		return self
 
-	def __exit__(self):
+	def __exit__(self, exc_type, exc_value, traceback):
 		self.disconnect()
 
 class Daemon(Thread):
@@ -86,7 +91,6 @@ class Daemon(Thread):
 	def __init__(self, serial):
 		Thread.__init__(self)
 		self.serial			= serial
-		self.serial.timeout	= 1 # 1 second timeout
 		self.queues_dict	= dict()
 		self.queues_lock	= RLock()
 		self.stop_event		= Event()
@@ -104,10 +108,12 @@ class Daemon(Thread):
 	def run(self):
 		raw_message = b''
 
-		while not self.stop_event.is_set():
+		while True:
 			byte = self.serial.read()
 			if byte == OUTPUT_START_BYTE or len(raw_message) > 0:
 				raw_message += byte
+			elif self.stop_event.is_set():
+				break
 
 			if len(raw_message) >= 2:
 				message			= raw_message[2:]
@@ -120,3 +126,10 @@ class Daemon(Thread):
 				# if message_length <= len(message):
 			# if len(raw_message) >= 2:
 		# while not self.stop_event.is_set():
+
+if __name__ == '__main__':
+	from pprint import pprint
+
+	with RobotCom('/dev/ttyUSB0') as ard0, RobotCom('/dev/ttyUSB1') as ard1:
+		print(ard0.get_uuid())
+		print(ard1.get_uuid())
