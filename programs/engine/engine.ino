29e5282f83d20ca48e1961109f3679fea81398f1
Timothe <timothe.frignac@yahoo.fr>
2016-09-22 14:35:49 +0200

    Ajout de commentaires indiquant à quoi servent les entrées/sorties.

--- programs/engine/engine.ino
+++ programs/engine/engine.ino
@@ -1,26 +1,43 @@
 #include <Arduino.h>
 #include <robotcom.h>
 
-#define CLK			2
-#define IN1_2		3
-#define PL4			4
-#define IN1_1		5
-#define EN1			6
-#define DATA		7
-#define SEL2		8
-#define EN2			9
-#define IN2_1		10
-#define IN2_2		11
-#define RSTDRV		12
-#define FAULTDRV	A0
-#define UDX			A1
-#define RSTX		A2
-#define RSTY		A3
-#define UDY			A4
-#define OF			A5
-#define SEL1		A6
-#define XY			A7
-#define SLPDRV		13
+//*************************************************************************
+//								PINOUT									  *
+//*************************************************************************
+
+// Motor control
+#define RSTDRV		12		// Reset motor driver. Logic low to reset
+#define SLPDRV		13		// Driver sleep mode. Logic low to enter sleep mode
+#define FAULTDRV	A0		// Fault condition driver output
+// Moteur 1 = Moteur gauche
+#define EN1			6		// ENable MOTOR 1 DRV8844
+#define IN1_1		5		// IN1 DRV8844 (MOTOR 1)
+#define IN1_2		3		// IN2 DRV8844 (MOTOR 1)
+// Moteur 2 = Moteur droit
+#define EN2			9		// ENable MOTOR 2 DRV8844
+#define IN2_1		10		// IN3 DRV8844 (MOTOR 2)
+#define IN2_2		11		// IN4 DRV8844 (MOTOR 2)
+//74HC165 register
+#define CLK			2		// clock signal for 74HC165 register
+#define DATA		7		// Serial data input from 74HC165 register
+#define PL			4		// PL for 74HC165 register
+// Quadrature decoder HCTL-2032
+#define RSTX		A2		// Active LOW. Clears the internal position counter and the position latch.
+#define RSTY		A3		// Active LOW. Clears the internal position counter and the position latch.
+// Counter byte selection
+// 		| MSB | 2ND | 3RD | LSB |
+// SEL1 |  0  |  1  |  0  |  1  |
+// SEL2 |  1  |  1  |  0  |  0  |
+#define SEL1		A6		 
+#define SEL2		8		
+#define OE			A5		// Logic LOW to enable the tri-states output buffers
+#define XY			A7		// Select the 1st or 2ND axis data to be read. LOW = X axis HIGH = Y axis
+// Signal informations
+#define UDX			A1		// Not used (must be used with CNTDEC and CNTCAS)
+#define UDY			A4		// Not used (must be used with CNTDEC and CNTCAS)
+
+
+
 
 void setup()
 {
@@ -28,7 +45,7 @@ void setup()
 
 /*	digitalWrite(RSTDRV, HIGH);
 	digitalWrite(SLPDRV, HIGH);//*/
-}
+
 
 void loop()
 {
@@ -56,5 +73,5 @@ void loop()
 
 	digitalWrite(IN2_1, LOW);
 	digitalWrite(IN2_2, HIGH);
-	analogWrite(EN2, PWM);//*/
+	analogWrite(EN2, PWM);*/
 }
