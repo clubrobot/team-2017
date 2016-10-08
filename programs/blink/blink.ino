a6f97ad0d96543b3e0264906ecb9835d6e05e836
Ulysse Darmet <darmet.ulysse@gmail.com>
2016-10-08 22:40:31 +0200

    Blink program is now working

--- programs/blink/blink.ino
+++ programs/blink/blink.ino
@@ -1,39 +1,22 @@
 #include <Arduino.h>
-#include <FrequencyTimer2.h>
 #include <robotcom.h>
 
 #define LED 13
 
-#define SETCOUNT	0x02
-#define SETPERIOD	0x03
+#define BLINK 0x02
 
 /* Prototypes */
 
-int setCountCommand(int argc, byte argv[], byte outv[]);
-int setPeriodCommand(int argc, byte argv[], byte outv[]);
-
-void toggleLedInterrupt();
-
-/* Global variables */
-
-volatile int count = 0;
-volatile int state = LOW;
-volatile unsigned long period = 2000000000;
+int blinkCommand(int argc, byte argv[], byte outv[]);
 
 /* Arduino */
 
 void setup()
 {
 	RobotCom::init();
-	RobotCom::addCommand(SETCOUNT, setCountCommand);
-	RobotCom::addCommand(SETPERIOD, setPeriodCommand);
+	RobotCom::addCommand(BLINK, blinkCommand);
 	
 	pinMode(LED, OUTPUT);
-	digitalWrite(LED, state);
-	
-	FrequencyTimer2::disable();
-	FrequencyTimer2::setPeriod(period);
-	FrequencyTimer2::setOnOverflow(toggleLedInterrupt);
 }
 
 void loop()
@@ -43,31 +26,15 @@ void loop()
 
 /* Commands */
 
-int setCountCommand(int argc, byte argv[], byte outv[])
+int blinkCommand(int argc, byte argv[], byte outv[])
 {
-	count += (argc > 1) ? argv[1] : 1;
-	return 0;
-}
-
-int setPeriodCommand(int argc, byte argv[], byte outv[])
-{
-	if (argc > 1 && argv[1] > 0)
+	int count = (argc > 1) ? argv[1] : 1;
+	for (int i = 0; i < count; i++)
 	{
-		FrequencyTimer2::setPeriod(period);
-		FrequencyTimer2::setOnOverflow(toggleLedInterrupt);
-	}
-	else
-	{
-		FrequencyTimer2::setOnOverflow(0);
+		digitalWrite(LED, HIGH);
+		delay(200);
+		digitalWrite(LED, LOW);
+		delay(200);
 	}
 	return 0;
 }
-
-/* Interrupts */
-
-void toggleLedInterrupt()
-{
-	state = !state;
-	digitalWrite(LED, state);
-}
-
