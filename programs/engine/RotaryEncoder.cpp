9f9e5a4a1ebaf343dc533d145a28fac376e6bfe5
Ulysse Darmet <darmet.ulysse@gmail.com>
2016-10-14 15:15:35 +0200

    Add a rotary encoder class

---
+++ programs/engine/RotaryEncoder.cpp
@@ -0,0 +1,69 @@
+#include "RotaryEncoder.h"
+
+RotaryEncoder::RotaryEncoder(Axis axis, int XY, int SEL1, int SEL2, int OE, int RST, int Q, int PL, int CP)
+:	m_counterAxis(axis)
+,	m_counterValue(0)
+
+,	m_counterAxisPin(XY)
+,	m_counterByteSelect1Pin(SEL1)
+,	m_counterByteSelect2Pin(SEL2)
+,	m_counterOutputEnablePin(OE)
+,	m_counterResetPin(RST)
+
+,	m_registerDataPin(Q)
+,	m_registerLatchPin(PL)
+,	m_registerShiftPin(CP)
+{
+	pinMode(m_counterAxisPin, OUTPUT);
+	pinMode(m_counterByteSelect1Pin, OUTPUT);
+	pinMode(m_counterByteSelect2Pin, OUTPUT);
+	pinMode(m_counterOutputEnablePin, OUTPUT);
+	pinMode(m_counterResetPin, OUTPUT);
+
+	pinMode(m_registerDataPin, INPUT);
+	pinMode(m_registerLatchPin, OUTPUT);
+	pinMode(m_registerShiftPin, OUTPUT);
+}
+
+void RotaryEncoder::updateCounter()
+{
+	digitalWrite(m_counterAxisPin, m_counterAxis);
+	digitalWrite(m_counterOutputEnablePin, LOW);
+	m_counterValue = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		delayMicroseconds(100); // TODO: is this necessary? Can we reduce it?
+		int sel1 = i & 0x01;
+		int sel2 = (~i >> 1) & 0x01;
+		digitalWrite(m_counterByteSelect1Pin, sel1);
+		digitalWrite(m_counterByteSelect2Pin, sel2);
+		m_counterValue <<= 8;
+		m_counterValue += readShiftRegister(m_registerDataPin, m_registerLatchPin, m_registerShiftPin);
+	}
+	digitalWrite(m_counterOutputEnablePin, HIGH);
+}
+
+void RotaryEncoder::resetCounter()
+{
+	// TODO: how to achieve this? Must read the datasheet!
+}
+
+long RotaryEncoder::getCounter() const
+{
+	return m_counterValue;
+}
+
+byte RotaryEncoder::readShiftRegister(int Q, int PL, int CP)
+{
+	digitalWrite(PL, HIGH);
+	byte value = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		value <<= 1;
+		value |= digitalRead(Q);
+		digitalWrite(CP, HIGH);
+		digitalWrite(CP, LOW);
+	}
+	digitalWrite(PL, LOW);
+	return value;
+}
---
+++ programs/engine/RotaryEncoder.h
@@ -0,0 +1,36 @@
+#ifndef __ROTARYENCODER_H__
+#define __ROTARYENCODER_H__
+
+#include <Arduino.h>
+
+class RotaryEncoder
+{
+public:
+
+	enum Axis {X = 0, Y = 1};
+
+	RotaryEncoder(Axis axis, int XY, int SEL1, int SEL2, int OE, int RST, int Q, int PL, int CP);
+
+	void updateCounter();
+	void resetCounter();
+	long getCounter() const;
+
+	static byte readShiftRegister(int Q, int PL, int CP);
+
+private:
+
+	Axis m_counterAxis;
+	long m_counterValue;
+
+	const int m_counterAxisPin;			// X = 0, Y = 1
+	const int m_counterByteSelect1Pin;	// MSB = 0, 2ND = 1, 3RD = 0, LSB = 1
+	const int m_counterByteSelect2Pin;	// MSB = 0, 2ND = 0, 3RD = 1, LSB = 1
+	const int m_counterOutputEnablePin;	// Active LOW. Enable the tri-states output buffers.
+	const int m_counterResetPin;		// Active LOW. Clear the internal position counter and the position latch.
+
+	const int m_registerDataPin;		// Serial data input from the 74HC165 register.
+	const int m_registerLatchPin;		// Active LOW. Latch signal for the 74HC165 register.
+	const int m_registerShiftPin;		// LOW-to-HIGH edge-triggered. Clock signal for the 74HC165 register.
+};
+
+#endif // __ROTARYENCODER_H__
