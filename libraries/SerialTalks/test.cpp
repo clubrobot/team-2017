cc386e73aa225fc324124c890ea1b5f70de1444c
Ulysse Darmet <darmet.ulysse@gmail.com>
2016-11-21 11:09:50 +0100

    Test RawInputBuffer

--- libraries/SerialTalks/test.cpp
+++ libraries/SerialTalks/test.cpp
@@ -4,7 +4,7 @@
 #include "RawInputBuffer.h"
 
 #define TEST_BUFFER          0
-#define TEST_RAWINPUTBUFFER  0
+#define TEST_RAWINPUTBUFFER  1
 #define TEST_RAWOUTPUTBUFFER 0
 
 using namespace std;
@@ -12,7 +12,7 @@ using namespace std;
 int main(int arc, char* argv[])
 {
 
-#ifdef TEST_BUFFER
+#if TEST_BUFFER
 	Buffer<4> buffer;
 	buffer.append(32);
 	buffer.append(12);
@@ -26,6 +26,25 @@ int main(int arc, char* argv[])
 	}
 	cout << endl;
 #endif // TEST_BUFFER
+
+#if TEST_RAWINPUTBUFFER
+	RawInputBuffer<6> buffer;
+	
+	int in_i0 = 123456789;
+	byte in_b0 = 123, in_b1 = 45;
+
+	buffer.append((byte*)(&in_i0), sizeof(in_i0));
+	buffer.append(in_b0);
+	buffer.append(in_b1);
+	cout << buffer.getSize() << endl;
+
+	int out_i0;
+	byte out_b0, out_b1;
+
+	buffer >> out_i0 >> out_b0 >> out_b1;
+	cout << out_i0 << " " << int(out_b0) << " " << int(out_b1) << endl;
+
+#endif // TEST_RAWINPUTBUFFER
 
 	return 0;
 }
