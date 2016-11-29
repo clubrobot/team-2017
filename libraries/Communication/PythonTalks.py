7b114bd0bd357ae09e81f6c3ff318a02f8012092
Enderdead <francois.gauthier-clerc@insa-rennes.fr>
2016-11-29 22:11:00 +0100

    suppression thread secondaire

--- libraries/Communication/PythonTalks.py
+++ libraries/Communication/PythonTalks.py
@@ -11,73 +11,80 @@ import pickle
 #création flux
 
 
-class PythonCom:
+class TCPTalks(Thread):
     def __init__(self,IP):
+        Thread.__init__(self)
+        self.MySocket2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.adresse = 0
+        self.port2 = 0
+        self.client = 0
+        self.server = 0
+        if os.name == 'nt':
+            self.host = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0]
+        if os.name == 'posix':
+            a = os.popen("""ifconfig | awk '/inet adr/ {gsub("adr:", "", $2); print $2}'""").readlines()
+            a.remove('127.0.0.1\n')
+            self.host = a[0][0:-1]
+
+        self.MySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if(IP != '0'):
+            try:
+                self.MySocket.bind((self.host,25565))
+            except socket.error:
+                print("error2")
             self.ordre = 1
             self.ip = IP
-            self.client = Client()
-            self.server = Server(25565)
+
 
 
         if(IP =='0'):
+            try:
+                self.MySocket.bind((self.host,25566))
+            except socket.error:
+                print("error2")
             self.ordre = 0
             self.ip = '0'
-            self.server= Server(25566)
-            self.client = Client()
+
 
     def send(self,variable):
         self.server.send(pickle.dumps(variable))
-        
+
     def connect(self):
         if(self.ordre == 0):
+            
             print("connection ...\nServer A ...")
-            self.server.connect()
-            while (self.server.getip()==0):
-                time.sleep(1)
-            self.ip = self.server.getip()
+            self.MySocket.listen(5)
+            self.server, self.adresse = self.MySocket.accept()  
+            self.ip = self.adresse[0]
             print("OK\nServer B ...")
             time.sleep(1)
-            self.client.connexion(self.ip,25565)
-            self.client.start()
-            self.server.stop()
+            self.port2 = 25565
+            self.connexion()
+            self.start()
             print("Ok\n connection established")
         if(self.ordre==1):
             print("connection ...\n Server A ...")
-            self.client.connexion(self.ip,25566)
-            self.client.start()
-            self.server.connect()
-            while (self.server.getip()==0):
-                time.sleep(1)
+            self.port2 = 25566
+            self.connexion()
+
+            self.start()
+            self.MySocket.listen(5)
+            self.server, self.adresse = self.MySocket.accept()  
+            self.ip = self.adresse[0]
             print("OK\nServer B ...")
 
-
-
-
-
-            
-class Client(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.ip = '0'
-        self.order = -1
-        self.MySocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-
-    def connexion(self, ip, port):
-        self.ip = ip 
-        self.port = port
+    def connexion(self):
         marqueur1 = 0
         marqueur2 = True
-        print(ip)
         while(marqueur2):
             marqueur2 = False
             try:
-                self.MySocket.connect((self.ip, self.port))
+                self.MySocket2.connect((self.ip, self.port2))
             except:
                 print('error1')
                 marqueur2 = True
                 marqueur1 = marqueur1 + 1
+                time.sleep(0.2)
             if(marqueur1 >5): 
                 return()
 
@@ -85,46 +92,15 @@ class Client(Thread):
         while True:
             
             time.sleep(1)
-            print(pickle.loads(self.MySocket.recv(8096)))
+            print(pickle.loads(self.MySocket2.recv(8096)))
             print("efse")
 
 
 
 
-class Server:
-    def __init__(self,port):
-        self.adresse = 0
-        self.port = port
-        self.client = 0
-        self.connexion = False
-
-        self.host = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0]
-        self.MySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            self.MySocket.bind((self.host,self.port))
-        except socket.error:
-            print("error2")
-
-    def getip(self):
-        if(self.connexion == False):
-            return(0)
-        else:
-            return(self.adresse[0])
-
-    def send(self,variable):
-        self.client.send(variable)
-
-    def stop(self): 
-        self.Terminated = True
-
-    def connect(self):
-        self.MySocket.listen(5)
-        self.client, self.adresse = self.MySocket.accept()    
-        #print("Client connécté , IP = %s et Port = %s" % (self.adresse[0] , self.adresse[1]))
-        self.connexion = True
 
 
-premier = PythonCom('10.12.39.51')
+premier = TCPTalks('192.168.1.14')
 premier.connect()
 premier.send("estest")
 time.sleep(5)
