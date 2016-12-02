35c96c8a610d9e8a599f6afe3778c5ea0aaf81d8
Ulysse Darmet <darmet.ulysse@gmail.com>
2016-12-02 23:43:09 +0100

    Add a bunch of new instructions

--- programs/wheeledbase/wheeledbase.ino
+++ programs/wheeledbase/wheeledbase.ino
@@ -7,9 +7,22 @@
 
 // Opcodes declaration
 
-#define SET_SPEED_OPCODE 0x02
-#define GET_STATE_OPCODE 0x03
-#define SET_STATE_OPCODE 0x04
+#define SET_MOTORS_SPEEDS_OPCODE 0x04
+
+#define MOVE_OPCODE              0x06
+#define GOTO_OPCODE              0x07
+
+#define SET_STATE_OPCODE         0x0A
+#define GET_STATE_OPCODE         0x0B
+#define GET_VELOCITIES_OPCODE    0x0C
+
+#define SET_PID_TUNINGS_OPCODE   0x0E
+#define GET_PID_TUNINGS_OPCODE   0x0F
+
+// PID controllers identifiers
+
+#define VELOCITY_CONTROLLER_IDENTIFIER 0x02
+#define OMEGA_CONTROLLER_IDENTIFIER    0x03
 
 
 // Load the different modules
@@ -19,26 +32,45 @@ WheeledBase	base; // See WheeledBase.cpp for initialization details
 Odometry	odometry(base);
 Control		control(base, odometry);
 
-// Commands
+// Instructions
 
-bool setSpeedCommand(Deserializer& input, Serializer& output)
+bool setMotorsSpeedsInstruction(Deserializer& input, Serializer& output)
 {
-	float leftSpeed, rightSpeed; // in mm/s
+	float leftSpeed, rightSpeed;
 	input >> leftSpeed >> rightSpeed;
+
+	control.disable();
 	base.leftMotor .setSpeed(leftSpeed);
 	base.rightMotor.setSpeed(rightSpeed);
-	control.disable();
+
 	return false;
 }
 
-bool getStateCommand(Deserializer& input, Serializer& output)
+bool moveInstruction(Deserializer& input, Serializer& output)
 {
-	const State& s = odometry.getState();
-	output << float(s.x) << float(s.y) << float(s.theta);
-	return true;
+	float velocity, omega;
+	input >> velocity >> omega;
+
+	control.enable();
+	control.setVelocitySetpoint(velocity);
+	control.setOmegaSetpoint   (omega);
+
+	talks.out << "moveInstruction";
+
+	return false;
 }
 
-bool setStateCommand(Deserializer& input, Serializer& output)
+bool gotoInstruction(Deserializer& input, Serializer& output)
+{
+	float x, y, theta;
+	input >> x >> y >> theta;
+
+	talks.err << "gotoInstruction: not implemented yet\n";
+	
+	return false;
+}
+
+bool setStateInstruction(Deserializer& input, Serializer& output)
 {
 	float x, y, theta;
 	input >> x >> y >> theta;
@@ -46,15 +78,76 @@ bool setStateCommand(Deserializer& input, Serializer& output)
 	return false;
 }
 
+bool getStateInstruction(Deserializer& input, Serializer& output)
+{
+	const State& s = odometry.getState();
+	output << float(s.x) << float(s.y) << float(s.theta);
+	return true;
+}
+
+bool getVelocitiesInstruction(Deserializer& input, Serializer& output)
+{
+	const Movement& m = odometry.getMovement();
+	output << float(m.velocity) << float(m.omega);
+	return true;
+}
+
+bool setPIDTuningsInstruction(Deserializer& input, Serializer& output)
+{
+	byte id;
+	float Kp, Ki, Kd;
+	input >> id >> Kp >> Ki >> Kd;
+	switch (id)
+	{
+	case VELOCITY_CONTROLLER_IDENTIFIER:
+		control.setVelocityControllerTunings(Kp, Ki, Kd);
+		return false;
+	case OMEGA_CONTROLLER_IDENTIFIER:
+		control.setOmegaControllerTunings(Kp, Ki, Kd);
+		return false;
+	default:
+		talks.err << "setPIDTuningInstruction: unknown PID controller identifier: " << id << "\n";
+		return false;
+	}
+}
+
+bool getPIDTuningsInstruction(Deserializer& input, Serializer& output)
+{
+	byte id;
+	input >> id;
+
+	switch (id)
+	{
+	case VELOCITY_CONTROLLER_IDENTIFIER:
+		output << control.getVelocityController().getKp();
+		output << control.getVelocityController().getKi();
+		output << control.getVelocityController().getKd();
+		return true;
+	case OMEGA_CONTROLLER_IDENTIFIER:
+		output << control.getOmegaController().getKp();
+		output << control.getOmegaController().getKi();
+		output << control.getOmegaController().getKd();
+		return true;
+	default:
+		talks.err << "getPIDTuningInstruction: unknown PID controller identifier: " << id << "\n";
+		return true;
+	}
+}
+
 void setup()
 {
 	talks.begin(Serial);
-	talks.attach(SET_SPEED_OPCODE, setSpeedCommand);
-	talks.attach(GET_STATE_OPCODE, getStateCommand);
-	talks.attach(SET_STATE_OPCODE, setStateCommand);
+	talks.attach(SET_MOTORS_SPEEDS_OPCODE, setMotorsSpeedsInstruction);
+	talks.attach(MOVE_OPCODE             , moveInstruction);
+	talks.attach(GOTO_OPCODE             , gotoInstruction);
+	talks.attach(SET_STATE_OPCODE        , setStateInstruction);
+	talks.attach(GET_STATE_OPCODE        , getStateInstruction);
+	talks.attach(GET_VELOCITIES_OPCODE   , getVelocitiesInstruction);
+	talks.attach(SET_PID_TUNINGS_OPCODE  , setPIDTuningsInstruction);
+	talks.attach(GET_PID_TUNINGS_OPCODE  , getPIDTuningsInstruction);
 
 	control.setVelocitySetpoint(0);
-	control.setOmegaSetpoint(0);
+	control.setOmegaSetpoint   (0);
 }
 
 void loop()
@@ -62,11 +155,7 @@ void loop()
 	talks.execute();
 
 	// Integrate odometry
-	odometry.update();//*
-	State    s = odometry.getState();
-	Movement m = odometry.getMovement();
-	//talks.out << s.x << " " << s.y << " " << s.theta << " ";
-	//talks.out << m.velocity << " " << m.omega << "\n";//*/
+	odometry.update();
 
 	// Integrate engineering control
 	control.step();
--- programs/wheeledbase/wheeledbase.py
+++ programs/wheeledbase/wheeledbase.py
@@ -3,9 +3,21 @@
 
 from serialtalks import *
 
-SET_SPEED_OPCODE = 0x02
-GET_STATE_OPCODE = 0x03
-SET_STATE_OPCODE = 0x04
+SET_MOTORS_SPEEDS_OPCODE = 0x04
+
+MOVE_OPCODE              = 0x06
+GOTO_OPCODE              = 0x07
+
+SET_STATE_OPCODE         = 0x0A
+GET_STATE_OPCODE         = 0x0B
+GET_VELOCITIES_OPCODE    = 0x0C
+
+SET_PID_TUNINGS_OPCODE   = 0x0E
+GET_PID_TUNINGS_OPCODE   = 0x0F
+
+VELOCITY_CONTROLLER_IDENTIFIER = 0x02
+OMEGA_CONTROLLER_IDENTIFIER    = 0x03
+
 
 
 class WheeledBase(SerialTalks):
@@ -13,11 +25,20 @@ class WheeledBase(SerialTalks):
 	def __init__(self, port = '/dev/arduino/wheeledbase'):
 		SerialTalks.__init__(self, port)
 
-	def setspeed(self, left, right):
-		self.send(SET_SPEED_OPCODE, FLOAT(left), FLOAT(right))
+	def set_motors_speeds(self, left, right):
+		self.send(SET_MOTORS_SPEEDS_OPCODE, FLOAT(left), FLOAT(right))
+
+	def move(self, velocity, omega):
+		self.send(MOVE_OPCODE, FLOAT(velocity), FLOAT(omega))
 
 	def stop(self):
-		self.setspeed(0, 0)
+		self.set_motors_speeds(0, 0)
+
+	def setstate(self, x, y, theta):
+		self.send(SET_STATE_OPCODE, FLOAT(x), FLOAT(y), FLOAT(theta))
+	
+	def reset(self):
+		self.setstate(0, 0, 0)
 
 	def getstate(self, timeout = None):
 		self.send(GET_STATE_OPCODE)
@@ -25,8 +46,29 @@ class WheeledBase(SerialTalks):
 		x, y, theta = output.read(FLOAT, FLOAT, FLOAT)
 		return x, y, theta
 	
-	def setstate(self, x, y, theta):
-		self.send(SET_STATE_OPCODE, FLOAT(x), FLOAT(y), FLOAT(theta))
+	def getvelocities(self, timeout = None):
+		self.send(GET_VELOCITIES_OPCODE)
+		output = self.poll(GET_VELOCITIES_OPCODE, timeout)
+		velocity, omega = output.read(FLOAT, FLOAT)
+		return velocity, omega
+
+	def settunings(self, identifier, Kp, Kd, Ki):
+		self.send(SET_PID_TUNINGS_OPCODE, BYTE(identifier), FLOAT(Kp), FLOAT(Kd), FLOAT(Ki))
 	
-	def resetstate(self):
-		self.setstate(0, 0, 0)
+	def set_velocity_tunings(self, Kp, Ki, Kd):
+		self.settunings(VELOCITY_CONTROLLER_IDENTIFIER, Kp, Ki, Kd)
+	
+	def set_omega_tunings(self, Kp, Ki, Kd):
+		self.settunings(OMEGA_CONTROLLER_IDENTIFIER, Kp, Ki, Kd)
+
+	def gettunings(self, identifier, timeout = None):
+		self.send(GET_PID_TUNINGS_OPCODE, BYTE(identifier))
+		output = self.poll(GET_PID_TUNINGS_OPCODE, timeout)
+		Kp, Kd, Ki = output.read(FLOAT, FLOAT, FLOAT)
+		return Kp, Kd, Ki
+
+	def get_velocity_tunings(self, timeout = None):
+		return self.gettunings(VELOCITY_CONTROLLER_IDENTIFIER, timeout)
+
+	def get_omega_tunings(self, timeout = None):
+		return self.gettunings(OMEGA_CONTROLLER_IDENTIFIER, timeout)
