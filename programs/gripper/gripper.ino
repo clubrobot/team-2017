ec87180343011fa446cfcf5927e53ca7dd873af9
Ulysse Darmet <darmet.ulysse@gmail.com>
2016-12-17 13:55:08 +0100

    Use the gripper class in an arduino sketch

---
+++ programs/gripper/gripper.ino
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <SerialTalks.h>
+
+#include "Gripper.h"
+
+// Opcodes declaration
+
+#define SET_GRIPPER_ENABLED  0x04
+#define SET_GRIPPER_POSITION 0x05
+
+
+// Load the gripper module
+
+//             | pin | min | max | idle | hold | drop |
+Gripper gripper(   5,    0,  164,   0.6,   0.2,   1.0 );
+
+// Instructions
+
+bool setGripperEnabledInstruction(Deserializer& input, Serializer& output)
+{
+	byte enabled;
+	input >> enabled;
+	if (enabled)
+		gripper.enable();
+	else
+		gripper.disable();
+}
+
+bool setGripperPositionInstruction(Deserializer& input, Serializer& output)
+{
+	float position;
+	input >> position;
+	gripper.enable();
+	gripper.setPosition(position);
+	return false;
+}
+
+void setup()
+{
+	talks.begin(Serial);
+	talks.attach(SET_GRIPPER_ENABLED , setGripperEnabledInstruction);
+	talks.attach(SET_GRIPPER_POSITION, setGripperPositionInstruction);
+}
+
+void loop()
+{
+	talks.execute();
+}
