1879be387e9284aef013c8309a859217fb6a908a
Ulysse Darmet <darmet.ulysse@gmail.com>
2016-12-17 13:55:18 +0100

    Add the python3 interface

---
+++ programs/gripper/gripper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+from serialtalks   import *
+from modulesrouter import Module
+
+SET_GRIPPER_ENABLED_OPCODE  = 0x04
+SET_GRIPPER_POSITION_OPCODE = 0x05
+
+
+class Gripper(Module):
+	def __init__(self, parent, uuid = 'gripper'):
+		Module.__init__(self, parent, uuid)
+
+	def enable(self):
+		self.send(SET_GRIPPER_ENABLED_OPCODE, BYTE(1))
+
+	def disable(self):
+		self.send(SET_GRIPPER_ENABLED_OPCODE, BYTE(0))
+
+	def setposition(self, position):
+		self.send(SET_GRIPPER_POSITION_OPCODE, FLOAT(position))
