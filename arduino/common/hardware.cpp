902392b7defd80d255ce0760005fabe88c7b8008
Ulysse Darmet <darmet.ulysse@gmail.com>
2017-01-11 21:44:29 +0100

    read shift register procedure

---
+++ arduino/common/hardware.cpp
@@ -0,0 +1,17 @@
+#include "hardware.h"
+
+
+byte readShiftRegister(int Q, int PL, int CP)
+{
+	digitalWrite(PL, HIGH);
+	byte value = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		value <<= 1;
+		value |= digitalRead(Q);
+		digitalWrite(CP, HIGH);
+		digitalWrite(CP, LOW);
+	}
+	digitalWrite(PL, LOW);
+	return value;
+}
---
+++ arduino/common/hardware.h
@@ -0,0 +1,9 @@
+#ifndef __HARDWARE_H__
+#define __HARDWARE_H__
+
+#include <Arduino.h>
+
+
+byte readShiftRegister(int Q, int PL, int CP);
+
+#endif // __HARDWARE_H__
