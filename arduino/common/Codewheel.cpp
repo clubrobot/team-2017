6dab92b285275b7330682c7039b9cc0ad6f0398a
Ulysse Darmet <darmet.ulysse@gmail.com>
2017-01-13 22:46:17 +0100

    Codewheels refactoring

---
+++ arduino/common/Codewheel.cpp
@@ -0,0 +1,79 @@
+#include "Codewheel.h"
+#include <math.h>
+#include "hardware.h"
+
+
+float Codewheel::getTraveledDistance()
+{
+	return (float)(m_currentCounter - m_startCounter) / m_countsPerRevolution * 2.0 * M_PI * m_radius;
+}
+
+float Codewheel::restart()
+{
+	float distance = getTraveledDistance();
+	m_startCounter = m_currentCounter;
+	return distance;
+}
+
+void Codewheel::attachCounter(int XY, int SEL1, int SEL2, int EN, int RST)
+{
+	m_COUNTER_XY   = XY;
+	m_COUNTER_SEL1 = SEL1;
+	m_COUNTER_SEL2 = SEL2;
+	m_COUNTER_OE   = OE;
+	m_COUNTER_RST  = RST;
+	pinMode(m_COUNTER_XY  , OUTPUT);
+	pinMode(m_COUNTER_SEL1, OUTPUT);
+	pinMode(m_COUNTER_SEL2, OUTPUT);
+	pinMode(m_COUNTER_OE  , OUTPUT);
+	pinMode(m_COUNTER_RST , OUTPUT);
+}
+
+void Codewheel::attachRegister(int DATA, int LATCH, int CLOCK)
+{
+	m_REGISTER_DATA  = DATA;
+	m_REGISTER_LATCH = LATCH;
+	m_REGISTER_CLOCK = CLOCK;
+	pinMode(m_REGISTER_DATA , INPUT);
+	pinMode(m_REGISTER_LATCH, OUTPUT);
+	pinMode(m_REGISTER_CLOCK, OUTPUT);
+}
+
+void Codewheel::setAxis(int axis)
+{
+	m_axis = axis;
+}
+
+void Codewheel::setRadius(float radius)
+{
+	m_radius = radius;
+}
+
+void Codewheel::setCountsPerRevolution(long countsPerRevolution)
+{
+	m_countsPerRevolution = countsPerRevolution;
+}
+
+void Codewheel::reset()
+{
+	digitalWrite(m_COUNTER_RST, LOW);
+	digitalWrite(m_COUNTER_RST, HIGH);
+}
+
+void Codewheel::update()
+{
+	digitalWrite(m_COUNTER_XY, m_axis);
+	digitalWrite(m_COUNTER_OE, LOW);
+	m_currentCounter = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		delayMicroseconds(100); // TODO: is this necessary? Can we reduce it?
+		int SEL1 = i & 0x01;
+		int SEL2 = (~i >> 1) & 0x01;
+		digitalWrite(m_SEL1, SEL1);
+		digitalWrite(m_SEL2, SEL2);
+		m_currentCounter <<= 8;
+		m_currentCounter += readShiftRegister(m_REGISTER_DATA, m_REGISTER_LATCH, m_REGISTER_CLOCK);
+	}
+	digitalWrite(m_COUNTER_OE, HIGH);
+}
---
+++ arduino/common/Codewheel.h
@@ -0,0 +1,47 @@
+#ifndef __CODEWHEEL_H__
+#define __CODEWHEEL_H__
+
+#include "NonCopyable.h"
+
+#define X 0
+#define Y 1
+
+
+class Codewheel : private NonCopyable
+{
+public:
+
+	float getTraveledDistance();
+	float restart();
+
+	void attachCounter(int XY, int SEL1, int SEL2, int OE, int RST);
+	void attachRegister(int DATA, int LATCH, int CLOCK);
+
+	void setAxis(int axis);
+	void setRadius(float radius);
+	void setCountsPerRevolution(long counts);
+
+	void reset();
+	void update();
+
+private:
+
+	long m_currentCounter;
+	long m_startCounter;
+
+	int m_COUNTER_XY;   // X = 0, Y = 1
+	int m_COUNTER_SEL1; // MSB = 0, 2ND = 1, 3RD = 0, LSB = 1
+	int m_COUNTER_SEL2; // MSB = 0, 2ND = 0, 3RD = 1, LSB = 1
+	int m_COUNTER_OE;   // Active LOW. Enable the tri-states output buffers.
+	int m_COUNTER_RST;  // Active LOW. Clear the internal position counter and the position latch.
+
+	int m_REGISTER_DATA;  // Serial data input from the 74HC165 register.
+	int m_REGISTER_LATCH; // Active LOW. Latch signal for the 74HC165 register.
+	int m_REGISTER_CLOCK; // LOW-to-HIGH edge-triggered. Clock signal for the 74HC165 register.
+
+	int   m_axis;
+	float m_radius;
+	long  m_countsPerRevolution;
+};
+
+#endif // __ROTARYENCODER_H__
