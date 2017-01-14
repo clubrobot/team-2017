f8683153b6011ef98230033bed819921ca944784
Ulysse Darmet <darmet.ulysse@gmail.com>
2017-01-14 15:55:38 +0100

    Move all mechanical constants into one "constants" header

---
+++ arduino/secondaryrobot/wheeledbase/constants.h
@@ -0,0 +1,24 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+// DC motors constants
+
+#define DCMOTOR_VELOCITY_CONSTANT 622 // RPM
+#define DCMOTOR_REDUCTION_RATIO    19
+#define DCMOTOR_SUPPLIED_VOLTAGE   11 // V
+
+// Codewheels constants
+
+#define CODEWHEELS_COUNTS_PER_REVOLUTION 10000
+
+// Mechanical constants
+
+#define LEFT_WHEEL_RADIUS   62.0 // mm
+#define RIGHT_WHEEL_RADIUS  62.0 // mm
+#define WHEELS_AXLE_TRACK  200.0 // mm
+
+#define LEFT_CODEWHEEL_RADIUS   22.7 // mm
+#define RIGHT_CODEWHEEL_RADIUS  23.0 // mm
+#define CODEWHEELS_AXLE_TRACK  318.0 // mm
+
+#endif // __CONSTANTS_H__
