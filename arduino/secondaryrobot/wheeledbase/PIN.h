c323a10dccf6e3388c8ab20d87ad4d9b570571d7
Ulysse Darmet <darmet.ulysse@gmail.com>
2017-01-19 17:13:08 +0100

    cosmetics

--- arduino/secondaryrobot/wheeledbase/PIN.h
+++ arduino/secondaryrobot/wheeledbase/PIN.h
@@ -11,7 +11,7 @@
 #define RIGHT_MOTOR_EN   9
 #define RIGHT_MOTOR_PWM 11
 #define RIGHT_MOTOR_DIR 10
-#define DRIVER_RESET	12
+#define DRIVER_RESET    12
 #define DRIVER_FAULT    A7
 
 // Codewheels
