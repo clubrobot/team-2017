b58cedc035667685828b378595ba4517932f71ba
Tif <timothe.frignac@yahoo.fr>
2017-02-07 18:47:27 +0100

    added display files

---
+++ arduino/secondaryrobot/display/pinmap.h
@@ -0,0 +1,17 @@
+#ifndef PINMAP_H
+#define PINMAP_H
+
+
+//ASCII TABLE => starts at @32
+ //a,b,c,d,e,f,g,dp   ASCII    dec
+ //                             0     ,     1     ,     2     ,     3     ,     4     ,     5     ,     6     ,     7     ,     8     ,     9 
+const byte segToDisplay[] { 0b11111100, 0b01100000, 0b11011010, 0b11110010, 0b01100110, 0b10110110, 0b10111110, 0b11100000, 0b11111110, 0b11110110};
+
+//Weight of each segment
+//                         a , b , c , d , e , f , g , dp
+const byte DISP[4][8]   {{128, 1 , 8 , 32, 64, 2 , 4 , 16},\
+                         {128, 1 , 4 , 16, 32, 64, 2 , 8 },\
+                         {128, 1 , 8 , 32, 64, 2 , 4 , 16},\
+                         {128, 1 , 4 , 16, 32, 64, 2 , 8 }};
+
+#endif //PINMAP_H
