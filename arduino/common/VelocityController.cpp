087636d7cfacd8275e17754c39c6bc1d19fd103c
Ulysse Darmet <darmet.ulysse@gmail.com>
2017-02-08 20:46:10 +0100

    Functional velocity ramps control

--- arduino/common/VelocityController.cpp
+++ arduino/common/VelocityController.cpp
@@ -15,42 +15,43 @@ void VelocityController::setMaximumDeccelerations(float linearDecceleration, flo
 	m_maxAngularDecceleration = angularDecceleration;
 }
 
+float VelocityController::generateRampSetpoint(float stepSetpoint, float input, float rampSetpoint, float acceleration, float decceleration, float timestep)
+{
+	// Do we have to accelerate or deccelerate to reach the desired setpoint?
+	if (input * (stepSetpoint - input) >= 0)
+		rampSetpoint += sign(stepSetpoint - input) * acceleration * timestep;
+	else
+		rampSetpoint += sign(stepSetpoint - input) * decceleration * timestep;
+
+	// If we are above the desired setpoint (i.e. the ramp), we no longer try to follow it.
+	// Instead we generate a new ramp starting from our current position.
+	if ((input - rampSetpoint) * (stepSetpoint - rampSetpoint) > 0)
+		rampSetpoint = input;
+
+	// We clamp the ramp so that it never exceeds the real setpoint
+	if ((stepSetpoint - input) * (stepSetpoint - rampSetpoint) < 0)
+		rampSetpoint = stepSetpoint;
+	
+	return rampSetpoint;
+}
+
 void VelocityController::process(float timestep)
 {
 	// Save setpoints
-	const float linearVelocitySetpoint  = m_linearSetpoint;
-	const float angularVelocitySetpoint = m_angularSetpoint;
+	const float savedLinearSetpoint  = m_linearSetpoint;
+	const float savedAngularSetpoint = m_angularSetpoint;
 
 	// Compute new setpoints
-	if (m_linearInput >= 0)
-	{
-		const float minLinearSetpoint = m_linearInput - m_maxLinearDecceleration * timestep;
-		const float maxLinearSetpoint = m_linearInput + m_maxLinearAcceleration  * timestep;
-		m_linearSetpoint = saturate(m_linearSetpoint, minLinearSetpoint, maxLinearSetpoint);
-	}
-	else
-	{
-		const float minLinearSetpoint = m_linearInput - m_maxLinearAcceleration  * timestep;
-		const float maxLinearSetpoint = m_linearInput + m_maxLinearDecceleration * timestep;
-		m_linearSetpoint = saturate(m_linearSetpoint, minLinearSetpoint, maxLinearSetpoint);
-	}
-	if (m_angularInput >= 0)
-	{
-		const float minAngularSetpoint = m_angularInput - m_maxAngularDecceleration * timestep;
-		const float maxAngularSetpoint = m_angularInput + m_maxAngularAcceleration  * timestep;
-		m_angularSetpoint = saturate(m_angularSetpoint, minAngularSetpoint, maxAngularSetpoint);
-	}
-	else
-	{
-		const float minAngularSetpoint = m_angularInput - m_maxAngularAcceleration  * timestep;
-		const float maxAngularSetpoint = m_angularInput + m_maxAngularDecceleration * timestep;
-		m_angularSetpoint = saturate(m_angularSetpoint, minAngularSetpoint, maxAngularSetpoint);
-	}
+	m_transitionalLinearVelocitySetpoint  = generateRampSetpoint(m_linearSetpoint,  m_linearInput,  m_transitionalLinearVelocitySetpoint,  m_maxLinearAcceleration,  m_maxLinearDecceleration,  timestep);
+	m_transitionalAngularVelocitySetpoint = generateRampSetpoint(m_angularSetpoint, m_angularInput, m_transitionalAngularVelocitySetpoint, m_maxAngularAcceleration, m_maxAngularDecceleration, timestep);
+//	m_transitionalAngularVelocitySetpoint = m_angularSetpoint;
 
 	// Do the engineering control
+	m_linearSetpoint  = m_transitionalLinearVelocitySetpoint;
+	m_angularSetpoint = m_transitionalAngularVelocitySetpoint;
 	DifferentialController::process(timestep);
 
 	// Restore setpoints
-	m_linearSetpoint  = linearVelocitySetpoint;
-	m_angularSetpoint = angularVelocitySetpoint;
+	m_linearSetpoint  = savedLinearSetpoint;
+	m_angularSetpoint = savedAngularSetpoint;
 }
