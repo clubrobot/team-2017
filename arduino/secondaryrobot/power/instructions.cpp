5f91fe7a36a3d3ca955f17d30b08a1d1ce3e2c7d
Tif <timothe.frignac@yahoo.fr>
2017-03-21 22:51:00 +0100

    Added Emergency stop instruction and get battery charge instruction

--- arduino/secondaryrobot/power/instructions.cpp
+++ arduino/secondaryrobot/power/instructions.cpp
@@ -1,19 +1,23 @@
 #include "instructions.h"
 #include "configuration.h"
 
+extern float voltage ;
+extern int percentage;
+
+// Return the emmergency button state (boolean value : 0 = Emergency stop; 1 = Normal)
 void GET_EMERGENCY_STOP_STATE(SerialTalks &inst, Deserializer &input, Serializer &output)
 {
-    
+    output << digitalRead(EMERGENCY_BUTTON_PIN);
 }
 
+// Return the battery voltage (float value)
 void GET_VOLTAGE(SerialTalks &inst, Deserializer &input, Serializer &output)
 {
-    float voltage = analogRead(VOLTAGE_PIN);
-	voltage = (voltage / VOLTAGE_MAX_NORMALIZED)*VOLTAGE_MAX;
-	output << voltage;
+   	output << voltage;
 }
 
+// Return the percentage of charge of the batery (int value)
 void GET_BATTERY_CHARGE(SerialTalks &inst, Deserializer &input, Serializer &output)
 {
-
+	output << percentage;
 }
--- arduino/secondaryrobot/power/power.ino
+++ arduino/secondaryrobot/power/power.ino
@@ -3,7 +3,10 @@
 #include "instructions.h"
 #include "../../common/SerialTalks.h"
 
+float voltage = 0;
+int percentage = 0;
 
+void getVoltage();
 
 void setup(){
     Serial.begin(SERIALTALKS_BAUDRATE);
@@ -14,14 +17,27 @@ void setup(){
     talks.bind(_GET_BATTERY_CHARGE_OPCODE, GET_BATTERY_CHARGE);
 
 	pinMode(CURRENT_PIN,INPUT);
-	pinMode(VOLTAGE_PIN,INPUT);  
+	pinMode(VOLTAGE_PIN,INPUT);
+	pinMode(EMERGENCY_BUTTON_PIN,INPUT);
+	pinMode(LED1,OUTPUT);
+	pinMode(LED2,OUTPUT);
+	pinMode(LED3,OUTPUT);
+	pinMode(LED4,OUTPUT);  
+
+	getVoltage();
     
 }
 
 void loop(){
      talks.execute();   
+	 getVoltage();
 }
 
+void getVoltage(){
+	voltage = analogRead(VOLTAGE_PIN);
+	voltage = (voltage / VOLTAGE_MAX_NORMALIZED)*VOLTAGE_MAX;
+	percentage = (voltage - VOLTAGE_MIN) *100 / (VOLTAGE_MAX - VOLTAGE_MIN);
+}
 
 
 
