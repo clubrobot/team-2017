c816eb8377d4f7b1504191b5c35eab85f4c68a33
Ulysse Darmet <darmet.ulysse@gmail.com>
2017-05-13 16:34:39 +0200

    Behavior now working

--- raspberrypi/behavior.py
+++ raspberrypi/behavior.py
@@ -11,8 +11,8 @@ class AccessDenied(RuntimeError): pass
 
 class Behavior(Manager):
 
-	def __init__(self, *args, **kwargs):
-		Manager.__init__(self, *args, timelimit=None, **kwargs)
+	def __init__(self, *args, timelimit=None, **kwargs):
+		Manager.__init__(self, *args, **kwargs)
 		self.timelimit = timelimit
 		self.whitelist = set()
 		self.blacklist = set()
@@ -32,18 +32,18 @@ class Behavior(Manager):
 				if thread_id in self.blacklist: self.blacklist.remove(thread_id)
 				if thread_id in self.whitelist: self.whitelist.remove(thread_id)
 		if not timelimit: self.whitelist.add(thread_id)
-		thrad._target = target
+		thread._target = target
 		thread.start()
 		return thread
 
 	def interrupt(self, thread):
-		if thread.is_alive:
+		if thread.is_alive():
 			thread_id = id(thread)
 			self.blacklist.add(thread_id)
 
 	def get(self, thread, timeout=None):
 		thread.join(timeout=timeout)
-		if thread.is_alive:
+		if thread.is_alive():
 			raise TimeoutError('timeout exceeded')
 		thread_id = id(thread)
 		output = self.outputs[thread_id]
@@ -56,7 +56,7 @@ class Behavior(Manager):
 		if thread_id in self.blacklist or (self.timelimit and elapsed_time > self.timelimit and thread_id not in self.whitelist):
 			raise AccessDenied(thread_id)
 		else:
-			Manager.send(self, *args, **kwargs)
+			return Manager.send(self, *args, **kwargs)
 
 	def make_decision(self):
 		raise RuntimeError("the 'make_decision' method must be overriden")
@@ -69,14 +69,14 @@ class Behavior(Manager):
 
 	def start(self):
 		self.starttime = time.monotonic()
-		while self.get_elapsed_time() < self.timelimit and not self.stop.is_set():
+		while (self.timelimit is None or self.get_elapsed_time() < self.timelimit) and not self.stop.is_set():
 			decision = self.make_decision()
 			procedure, args, kwargs, location = decision
 			if procedure is None:
 				time.sleep(1)
 				continue
 			if location is not None:
-				goto = self.perform(self.goto_procedure, args=location)
+				goto = self.perform(self.goto_procedure, args=(location,))
 				success = self.get(goto)
 			else:
 				success = True
@@ -86,4 +86,7 @@ class Behavior(Manager):
 		self.perform(stop_procedure, timelimit=False)
 
 	def get_elapsed_time(self):
-		return time.monotonic() - self.starttime
+		if hasattr(self, 'starttime'):
+			return time.monotonic() - self.starttime
+		else:
+			return 0
