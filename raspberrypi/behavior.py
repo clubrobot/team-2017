f429a602ddd0c2e85c466482eae1e48c092dd7ea
Ulysse Darmet <darmet.ulysse@gmail.com>
2017-05-13 12:33:12 +0200

    Small refactoring. Not tested yet.

--- raspberrypi/behavior.py
+++ raspberrypi/behavior.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import time
-from threading import Thread, current_thread
+from threading import Thread, Event, current_thread
 
 from components import Manager
 
@@ -16,49 +16,74 @@ class Behavior(Manager):
 		self.timelimit = timelimit
 		self.whitelist = set()
 		self.blacklist = set()
+		self.outputs = dict()
+		self.stop = Event()
+
+	def perform(self, procedure, args=(), kwargs={}, timelimit=True):
+		thread = Thread(args=args, kwargs=kwargs, daemon=True)
+		thread_id = id(thread)
+		def target(*args, **kwargs):
+			try:
+				output = procedure(*args, **kwargs)
+				self.outputs[thread_id] = output
+			except AccessDenied:
+				self.outputs[thread_id] = None
+			finally:
+				if thread_id in self.blacklist: self.blacklist.remove(thread_id)
+				if thread_id in self.whitelist: self.whitelist.remove(thread_id)
+		if not timelimit: self.whitelist.add(thread_id)
+		thrad._target = target
+		thread.start()
+		return thread
+
+	def interrupt(self, thread):
+		if thread.is_alive:
+			thread_id = id(thread)
+			self.blacklist.add(thread_id)
+
+	def get(self, thread, timeout=None):
+		thread.join(timeout=timeout)
+		if thread.is_alive:
+			raise TimeoutError('timeout exceeded')
+		thread_id = id(thread)
+		output = self.outputs[thread_id]
+		del self.outputs[thread_id]
+		return output
 
 	def send(self, *args, **kwargs):
 		thread_id = id(current_thread())
 		elapsed_time = self.get_elapsed_time()
 		if thread_id in self.blacklist or (self.timelimit and elapsed_time > self.timelimit and thread_id not in self.whitelist):
-			self.blacklist.remove(thread)
-			raise AccessDenied()
+			raise AccessDenied(thread_id)
 		else:
 			Manager.send(self, *args, **kwargs)
 
+	def make_decision(self):
+		raise RuntimeError("the 'make_decision' method must be overriden")
+
+	def goto_procedure(self, destination):
+		raise RuntimeError("the 'goto_procedure' method must be overriden")
+
+	def stop_procedure(self):
+		pass
+
 	def start(self):
 		self.starttime = time.monotonic()
-		# TODO: start actions
+		while self.get_elapsed_time() < self.timelimit and not self.stop.is_set():
+			decision = self.make_decision()
+			procedure, args, kwargs, location = decision
+			if procedure is None:
+				time.sleep(1)
+				continue
+			if location is not None:
+				goto = self.perform(self.goto_procedure, args=location)
+				success = self.get(goto)
+			else:
+				success = True
+			if success:
+				action = self.perform(procedure, args=args, kwargs=kwargs)
+				self.get(action)
+		self.perform(stop_procedure, timelimit=False)
 
 	def get_elapsed_time(self):
 		return time.monotonic() - self.starttime
-
-	def deny_access(self, threadname):
-		self.blacklist.add(threadname)
-
-
-class Movement(Thread):
-
-	def __init__(self, behavior):
-		Thread.__init__(self)
-		self.dameon = True
-		self.behavior = behavior
-	
-	def run(self):
-		try:
-			self.perform()
-		except AccessDenied: pass
-
-	def perform(self):
-		raise RuntimeError("the 'perform' method must be overriden")
-
-	def interrupt(self, join=False):
-		if self.is_alive:
-			thread_id = id(self)
-			self.behavior.deny_access(thread_id)
-			if join:
-				self.join()
-
-
-class Action():
-	pass # TODO:
